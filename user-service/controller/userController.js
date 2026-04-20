dffc9ec5b0753e2f5c556178c0614ad6af44788f
Anas <anas19257@gmail.com>
2026-04-20 21:37:42 +0530

    Fixed Prisma

--- user-service/controller/userController.js
+++ user-service/controller/userController.js
@@ -1,7 +1,11 @@
 const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+});
 
+const prisma = new PrismaClient({ adapter });
 
 const getAllUsers = async (req, res) => {
     try {
--- user-service/index.js
+++ user-service/index.js
@@ -1,17 +1,17 @@
+require('dotenv').config({ path: './.env' })
 const express = require('express')
 const cors = require('cors')
 const { postgresConnect } = require('./config/postgresConfig')
 const { addLog } = require('./config/logConfig')
-const {userRouter} = require('./router/userRouter')
-const dotenv = require('dotenv')
+const userRouter = require('./router/userRouter')
 
-const dt = dotenv.config()
 
 const app =  express();
-
-app.use(cors)
+const PORT = process.env.USER_SERVICE_PORT || 3001 
+app.use(cors())
+app.use(express.json())
 app.use('/api/users',userRouter)
 
-app.listen(dt.parsed.USER_SERVICE_PORT , () => {
-    console.log("server started on " + dt.parsed.USER_SERVICE_PORT)
+app.listen(PORT , () => {
+    console.log("server started on " + PORT)
 })
