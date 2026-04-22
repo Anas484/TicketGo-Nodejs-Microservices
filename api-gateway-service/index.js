ff7baa1340f78e7672cfd036fc53723388e20ecc
Anas <anas19257@gmail.com>
2026-04-22 13:04:57 +0530

    Added Api-Gateway service

---
+++ api-gateway-service/index.js
@@ -0,0 +1,33 @@
+
+require('dotenv').config();
+const express = require('express');
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+
+const app = express();
+const PORT = process.env.API_GATEWAY_PORT || 3000;
+
+app.use("/users", createProxyMiddleware({
+  target: process.env.USER_SERVICE_URL,
+  changeOrigin: true,
+}));
+
+app.use("/events", createProxyMiddleware({
+  target: process.env.EVENT_SERVICE_URL,
+  changeOrigin: true,
+}));
+
+app.use("/bookings", createProxyMiddleware({
+  target: process.env.BOOKING_SERVICE_URL,
+  changeOrigin: true,
+}));
+
+// app.use("/payments", createProxyMiddleware({
+//   target: process.env.PAYMENT_SERVICE_URL,
+//   changeOrigin: true,
+// }));
+
+
+app.listen(PORT, () => {
+    console.log(`API Gateway service started on port ${PORT}`);
+});
