526c2e0b2caf583ac1384d8fefba756b318aa2c1
Anas <anas19257@gmail.com>
2026-04-24 09:07:28 +0530

    Added rate limiter

--- api-gateway-service/index.js
+++ api-gateway-service/index.js
@@ -2,10 +2,18 @@
 require('dotenv').config();
 const express = require('express');
 const {createProxyMiddleware} = require('http-proxy-middleware');
+const ratelimit = require('express-rate-limit');
 
+
+const limiter = ratelimit({
+  windowMs: 10 * 60 * 1000, 
+  max: 100 
+});
 
 const app = express();
 const PORT = process.env.API_GATEWAY_PORT || 3000;
+
+app.use(limiter);
 
 app.use("/users", createProxyMiddleware({
   target: process.env.USER_SERVICE_URL,
